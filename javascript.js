06b21fb72c56f1be5ff3956e09945975dde233f7
bhuviesha <139739026+bhuviesha@users.noreply.github.com>
2024-07-31 19:45:15 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+	event.preventDefault();
+	// Add login validation and authentication logic here
+	alert("Login successful!");
+});
